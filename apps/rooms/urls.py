29734bb9e6b398e67bc62259abeeaa80b034b167
Josh Bright <josh.bright@gmail.com>
2021-06-29 22:40:47 -0700

    We now populate the list of floors on page load using vue

--- apps/rooms/urls.py
+++ apps/rooms/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -6,4 +6,5 @@ urlpatterns = [
 	path('', views.index, name='rooms-index'),
 	path('setup/', views.setup, name='rooms-setup'),
 	path('ajax/create-floor/', views.ajax_create_floor, name='ajax-rooms-create-floor'),
+	path('api/', include('rooms.api.urls')),
 ]
--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'apps.home',
     'apps.core',
     'apps.rooms',
