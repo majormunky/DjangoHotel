a6117ae24450e27f81b97f72b7c6bbcde1f0465a
Josh Bright <josh.bright@gmail.com>
2021-06-29 20:16:47 -0700

    Added a setup page

--- apps/rooms/urls.py
+++ apps/rooms/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='rooms-index'),
+	path('setup/', views.setup, name='rooms-setup'),
 ]
--- apps/rooms/views.py
+++ apps/rooms/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "rooms/index.html", {})
+
+
+def setup(request):
+	return render(request, "rooms/setup.html", {})
