a0b27135ddfd685ef7513c6e7cfefa3c4c3c858e
Josh Bright <josh.bright@gmail.com>
2021-06-30 21:53:32 -0700

    Added auth and permission checks to the rooms api view

--- apps/rooms/api/views.py
+++ apps/rooms/api/views.py
@@ -1,10 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rooms.api import serializers
 from rooms import models
 
 
 class ListFloors(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         floors = serializers.FloorSerializer(models.Floor.objects.all(), many=True)
         return Response(floors.data)
