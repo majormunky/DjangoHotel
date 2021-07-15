4a7238c8e3412dbefe5f52b0d08152df46502fc1
Josh Bright <josh.bright@gmail.com>
2021-07-14 19:55:51 -0700

    Added booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_active = models.BooleanField()
+    check_in_time = models.DateTimeField()
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
