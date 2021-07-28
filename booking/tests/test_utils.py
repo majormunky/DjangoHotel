7152d43f44230083167c35b356c17c2f6ea947a1
Josh Bright <josh.bright@gmail.com>
2021-07-27 20:44:47 -0700

    Added test to check on getting rooms when we have no overlap of dates

--- booking/tests/test_utils.py
+++ booking/tests/test_utils.py
@@ -52,8 +52,17 @@ def booking_list():
 
 
 @pytest.mark.django_db
-def test_check_bookings_with_date_range(booking_list):
+def test_check_bookings_with_date_range_with_some_results(booking_list):
     start_date = datetime.datetime(2021, 8, 12).date()
     end_date = datetime.datetime(2021, 8, 16).date()
     result = utils.check_bookings_with_date_range(start_date, end_date)
     assert len(result) == 2
+
+
+@pytest.mark.django_db
+def test_checking_bookings_with_date_range_no_overlap(booking_list):
+    start_date = datetime.datetime(2021, 10, 12).date()
+    end_date = datetime.datetime(2021, 10, 16).date()
+    result = utils.check_bookings_with_date_range(start_date, end_date)
+    print(result[0])
+    assert len(result) == 3
