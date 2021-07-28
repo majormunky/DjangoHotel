75a251478a82bf8f98d22afb1892182c8f74760f
Josh Bright <josh.bright@gmail.com>
2021-07-27 21:39:02 -0700

    Added a room serializer

--- rooms/api/serializers.py
+++ rooms/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rooms import models
 
+
 class FloorSerializer(serializers.ModelSerializer):
     rooms = serializers.SerializerMethodField()
 
@@ -10,3 +11,9 @@ class FloorSerializer(serializers.ModelSerializer):
 
     def get_rooms(self, obj):
         return obj.room_set.count()
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Room
+        fields = ["id", "number", "floor", "bed_count", "beds", "size"]
