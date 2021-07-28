d0a19b216653f33db9ce2239d9465405738d94a8
Josh Bright <josh.bright@gmail.com>
2021-07-27 20:48:12 -0700

    Cleaned up file

--- rooms/views.py
+++ rooms/views.py
@@ -4,27 +4,25 @@ from . import models
 
 
 def index(request):
-	
-	return render(request, "rooms/index.html", {})
+    return render(request, "rooms/index.html", {})
 
 
 def setup(request):
-	floor_list = models.Floor.objects.all().order_by("number")
-	return render(request, "rooms/setup.html", {"floor_list": floor_list})
+    floor_list = models.Floor.objects.all().order_by("number")
+    return render(request, "rooms/setup.html", {"floor_list": floor_list})
 
 
 # Ajax views
 
+
 def ajax_create_floor(request):
-	if not request.is_ajax():
-		return JsonResponse({"result": "failed", "message": "Invalid Request"})
+    if not request.is_ajax():
+        return JsonResponse({"result": "failed", "message": "Invalid Request"})
+    floor_num = request.POST.get("floor-number", None)
+    if floor_num is None:
+        return JsonResponse({"result": "failed", "message": "Missing Floor Number"})
 
-	print(request.POST)
-	floor_num = request.POST.get("floor-number", None)
-	if floor_num is None:
-		return JsonResponse({"result": "failed", "message": "Missing Floor Number"})
+    new_floor = models.Floor(number=floor_num)
+    new_floor.save()
 
-	new_floor = models.Floor(number=floor_num)
-	new_floor.save()
-
-	return JsonResponse({"result": "success", "floor_id": new_floor.id})
+    return JsonResponse({"result": "success", "floor_id": new_floor.id})
