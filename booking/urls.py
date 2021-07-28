9ad50ee23a773ad766a34f1220789eb84ad9d4a7
Josh Bright <josh.bright@gmail.com>
2021-07-27 17:33:09 -0700

    Added an ajax view to check if we can create a booking or not, checking different ways on how we can determine an open room based upon date ranges

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("book-room/", views.book_room, name="book-room"),
+    path("ajax-check-booking/", views.ajax_check_booking, name="ajax-check-booking"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,28 @@
+import datetime
 from django.shortcuts import render
+from django.http import JsonResponse
+from rooms import models as room_models
 
 
 def book_room(request):
     return render(request, "booking/book-room.html", {})
+
+
+def ajax_check_booking(request):
+    start_date = request.POST.get("start", None)
+    end_date = request.POST.get("end", None)
+
+    if not all([start_date, end_date]):
+        return JsonResponse(
+            {"result": "failed", "message": "Missing start or end date"}
+        )
+
+    start_date_obj = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end_date_obj = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    # simple but probably slow way to find an available room
+    room_list = room_models.Room.objects.filter(booking__isnull=True)
+
+    print(room_list)
+
+    return JsonResponse({"result": "success"})
