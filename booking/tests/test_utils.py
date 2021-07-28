a10003a356c8c56c1027ffb8ebd783505d565def
Josh Bright <josh.bright@gmail.com>
2021-07-27 20:12:00 -0700

    Added test to check if our utils function works that gets an available room

--- booking/tests/test_utils.py
+++ booking/tests/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 import datetime
+import pytz
 from mixer.backend.django import mixer
 from rooms import models as room_models
 from booking import models
@@ -8,25 +9,40 @@ from booking import utils
 
 @pytest.fixture
 def booking_list():
-    booking1 = mixer.blend(models.Booking)
+    tz = pytz.timezone("US/Pacific")
+    b1_start_date = datetime.datetime(2021, 8, 1, 17, 0, 0).astimezone(tz)
+    b1_end_date = datetime.datetime(2021, 8, 10, 11, 0, 0).astimezone(tz)
+    room1 = mixer.blend(room_models.Room)
+    room1.save()
+    booking1 = mixer.blend(
+        models.Booking,
+        start_date=b1_start_date,
+        end_date=b1_end_date,
+        scheduled_room=room1,
+    )
     booking1.save()
 
-    booking2 = mixer.blend(models.Booking)
+    b2_start_date = datetime.datetime(2021, 8, 4, 17, 0, 0).astimezone(tz)
+    b2_end_date = datetime.datetime(2021, 8, 15, 11, 0, 0).astimezone(tz)
+    room2 = mixer.blend(room_models.Room)
+    room2.save()
+    booking2 = mixer.blend(
+        models.Booking,
+        start_date=b2_start_date,
+        end_date=b2_end_date,
+        scheduled_room=room2,
+    )
     booking2.save()
+
+    room3 = mixer.blend(room_models.Room)
+    room3.save()
 
     return models.Booking.objects.all()
 
 
-@pytest.fixture
-def room_list():
-    room1 = mixer.blend(room_models.Room)
-    room2 = mixer.blend(room_models.Room)
-    return room_models.Room.objects.all()
-
-
 @pytest.mark.django_db
-def test_check_bookings_with_date_range(room_list):
-    start_date = datetime.datetime.today().date()
-    end_date = datetime.datetime.today().date()
+def test_check_bookings_with_date_range(booking_list):
+    start_date = datetime.datetime(2021, 8, 12).date()
+    end_date = datetime.datetime(2021, 8, 16).date()
     result = utils.check_bookings_with_date_range(start_date, end_date)
-    assert result.count() == 2
+    assert len(result) == 2
