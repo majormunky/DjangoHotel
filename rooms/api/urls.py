1e9d2cb45e01c2cd07bd728e16794f98ee5cf273
Josh Bright <josh.bright@gmail.com>
2021-07-28 18:29:11 -0700

    Added api view to generate rooms for a floor

--- rooms/api/urls.py
+++ rooms/api/urls.py
@@ -5,4 +5,9 @@ from rooms.api import serializers
 urlpatterns = [
     path("floors/", views.ListFloors.as_view(), name="api-floor-list"),
     path("rooms/", views.RoomsForFloorAPIView.as_view(), name="api-rooms-for-floor"),
+    path(
+        "floors/generate-rooms/",
+        views.GenerateRoomsForFloorAPIView.as_view(),
+        name="api-generate-rooms",
+    ),
 ]
--- rooms/api/views.py
+++ rooms/api/views.py
@@ -28,3 +28,26 @@ class RoomsForFloorAPIView(APIView):
         room_list = models.Room.objects.filter(floor=floor_data.first())
         json_data = serializers.RoomSerializer(room_list, many=True).data
         return Response(json_data)
+
+
+class GenerateRoomsForFloorAPIView(APIView):
+    def post(self, request, format=None):
+        floor_num = request.POST.get("floor_num")
+        room_count = int(request.POST.get("room_count"))
+        floor_obj = models.Floor.objects.filter(number=floor_num)
+        rooms_created = 0
+        if floor_obj:
+            floor_obj = floor_obj.first()
+            for i in range(1, room_count + 1):
+                room_obj, created = models.Room.objects.get_or_create(
+                    number=str(i),
+                    floor=floor_obj,
+                    defaults={"bed_count": 0, "size": "small"},
+                )
+                if created:
+                    print(room_obj)
+                    rooms_created += 1
+                else:
+                    print("found existing room", room_obj)
+            return Response({"result": "success", "floors_created": rooms_created})
+        return Response({"result": "failed", "message": "Unable to find floor object"})
