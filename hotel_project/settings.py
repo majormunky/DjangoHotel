32d5e6b356d3e5bd5e4b5f9876b62953a0633697
Josh Bright <josh.bright@gmail.com>
2021-08-25 19:38:17 -0700

    Instead of using a local settings file, we now use python-dotenv to load in environment variables

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import sys
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from hotel_project.local_settings import *
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DJANGO_DEBUG"] == "True"
 
 ALLOWED_HOSTS = []
 
