54ad006d459cd4f4fdfc4671baec95276399695e
Josh Bright <josh.bright@gmail.com>
2021-08-25 22:20:07 -0700

    Adjusted the bed model to not use a choices list, and to have a description field

--- rooms/models.py
+++ rooms/models.py
@@ -48,13 +48,8 @@ class Bed(models.Model):
     name: Description of the bed
     """
 
-    bed_choices = (
-        ("single", "Single"),
-        ("twin", "Twin"),
-        ("queen", "Queen"),
-        ("king", "King"),
-    )
-    name = models.CharField(max_length=64, choices=bed_choices)
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.get_name_display()
