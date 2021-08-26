5da8450d8e8c683e28c7af1b4de20024ce54b9b7
Josh Bright <josh.bright@gmail.com>
2021-08-25 21:54:16 -0700

    Added a room serializer.  We now serialize the room data when we create a new room and send it back to the client

--- rooms/api/serializers.py
+++ rooms/api/serializers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse
 from rest_framework import serializers
 from rooms import models
 
@@ -10,3 +11,13 @@ class FloorSerializer(serializers.ModelSerializer):
 
     def get_rooms(self, obj):
         return obj.room_set.count()
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+    class Meta:
+        model = models.Room
+        fields = ["id", "number", "size", "bed_count", "url"]
+
+    def get_url(self, obj):
+        return reverse("dashboard-room-detail", args=[obj.id])
