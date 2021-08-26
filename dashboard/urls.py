6162deb70272188e92337ca824d49ebfa500bed8
Josh Bright <josh.bright@gmail.com>
2021-08-25 20:03:03 -0700

    Added a floor setup dashboard page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from dashboard import views
 
 urlpatterns = [
 	path("", views.index, name="dashboard-index"),
+	path("floor-setup/", views.floor_setup, name="dashboard-floor-setup"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, "dashboard/index.html", {})
+
+
+def floor_setup(request):
+	return render(request, "dashboard/floor_setup.html", {})
