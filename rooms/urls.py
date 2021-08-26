8c58dae207aa705f4b0150f8ae27b9b75fef0a50
Josh Bright <josh.bright@gmail.com>
2021-08-25 21:35:17 -0700

    We can now add new rooms to a floor, but we don't write out a new table row after success (yet)

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('dashboard/ajax/create-floor/', views.ajax_create_floor, name='rooms-ajax-create-floor'),
+	path('dashboard/ajax/create-room/', views.ajax_create_room, name='rooms-ajax-create-room'),
 	path('rooms/api/', include('rooms.api.urls')),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, reverse
 from django.http import JsonResponse
 from . import models
+from core import utils as core_utils
 
 
 def index(request):
-	
 	return render(request, "rooms/index.html", {})
 
 
@@ -18,7 +18,6 @@ def ajax_create_floor(request):
 	if not request.is_ajax():
 		return JsonResponse({"result": "failed", "message": "Invalid Request"})
 
-	print(request.POST)
 	floor_num = request.POST.get("floor-number", None)
 	if floor_num is None:
 		return JsonResponse({"result": "failed", "message": "Missing Floor Number"})
@@ -27,3 +26,30 @@ def ajax_create_floor(request):
 	new_floor.save()
 
 	return JsonResponse({"result": "success", "data": {"floor_id": new_floor.id, "number": new_floor.number, "url": reverse("dashboard-floor-detail", args=new_floor.id)}})
+
+
+def ajax_create_room(request):
+	if not request.is_ajax():
+		return JsonResponse({"result": "failed", "message": "Invalid Request"})
+
+	floor_id = request.POST.get("floor-id", None)
+	room_number = request.POST.get("room-number", None)
+	room_size = request.POST.get("room-size", None)
+
+	if not all([floor_id, room_number, room_size]):
+		return JsonResponse({"result": "failed", "message": "Missing Data"})
+
+	floor_data = core_utils.get_object_or_none(models.Floor, floor_id)
+
+	if floor_data is None:
+		return JsonResponse({"result": "failed", "message": "Unable to find floor with provided id: {}".format(floor_id)})
+
+	new_room = models.Room(
+		number=room_number,
+		floor=floor_data,
+		bed_count=0,
+		size=room_size
+	)
+	new_room.save()
+
+	return JsonResponse({"result": "success"})
