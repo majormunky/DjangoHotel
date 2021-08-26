facc15cc31e54a08afd5ff97032f7a1d2cf2da5b
Josh Bright <josh.bright@gmail.com>
2021-08-25 19:47:39 -0700

    Created a new dashboard app to power the dashboard instead of using the rooms app.

--- hotel_project/urls.py
+++ hotel_project/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", include("dashboard.urls")),
     path("", include("core.urls")),
-    path("", include("rooms.urls")),
     path("", include("home.urls")),
     path("", include("booking.urls")),
 ]
