d2c0cd0a9bd81fbf2afc9328726d37ea9a0c4214
Josh Bright <josh.bright@gmail.com>
2022-02-05 11:11:01 -0800

    More file renaming

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ from dashboard import views
 urlpatterns = [
     path("dashboard/", views.index, name="dashboard-index"),
     path("dashboard/setup/", views.setup, name="dashboard-setup"),
-    path("dashboard/bookings/", views.bookings, name="dashboard-booking"),
+    path("dashboard/bookings/", views.bookings_list, name="dashboard-booking"),
     path(
         "dashboard/booking/create/",
         views.create_booking,
