f6645e087cdace8b2a143683bff526693efd707d
Josh Bright <josh.bright@gmail.com>
2022-02-05 10:39:44 -0800

    Updated crispy form settings on the booking form

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,28 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from booking import models
 
 
 class BookingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = "POST"
+        self.helper.form_action = "."
+        self.helper.layout = Layout(
+            Fieldset(
+                '',
+                'user',
+                'start_date',
+                'end_date',
+                'scheduled_room',
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='btn btn-sm btn-primary')
+            )
+        )
+
     class Meta:
         model = models.Booking
         fields = [
