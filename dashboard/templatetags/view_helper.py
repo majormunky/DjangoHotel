6f37dde286c1cd3bbad70f84538d16096b0dbfe4
Josh Bright <josh.bright@gmail.com>
2022-02-05 14:19:49 -0800

    We now color in table cells depending if its taken or not for that day on the room finder page

--- dashboard/templatetags/view_helper.py
+++ dashboard/templatetags/view_helper.py
@@ -1,3 +1,4 @@
+from django.utils.safestring import mark_safe
 from django import template
 
 register = template.Library()
@@ -12,5 +13,5 @@ def room_status_for_date(context, date_obj, room_obj):
     if len(room_dict[date_key]):
         for room in room_dict[date_key]:
             if room_key == str(room):
-                return "*"
-    return "-"
+                return mark_safe("<td class='room-booked'>*</td>")
+    return mark_safe("<td class='room-vacant'>-</td>")
