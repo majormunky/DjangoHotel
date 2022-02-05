e9ca636a02b9b3d98ca15b52301864b1c20f03b0
Josh Bright <josh.bright@gmail.com>
2022-02-05 14:01:52 -0800

    Added a view helper for custom template tags and filters

---
+++ dashboard/templatetags/view_helper.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def room_status_for_date(context, date_obj, room_obj):
+    date_key = date_obj.strftime("%m-%d-%Y")
+    room_dict = context["room_schedule"]
+    room_key = str(room_obj)
+
+    if len(room_dict[date_key]):
+        for room in room_dict[date_key]:
+            if room_key == str(room):
+                return "*"
+    return "-"
