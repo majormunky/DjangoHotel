f50714ab27affc3bbd459dd96ba9195d8b3abc9f
Josh Bright <josh.bright@gmail.com>
2022-02-05 10:45:46 -0800

    Updated start and end date fields to be date fields

--- booking/models.py
+++ booking/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 class Booking(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     is_active = models.BooleanField()
     check_in_time = models.DateTimeField(blank=True, null=True)
     room = models.OneToOneField(
