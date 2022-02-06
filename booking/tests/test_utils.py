1526c5ad47b74b281fbeac367a9aaa5bc12574c6
Josh Bright <josh.bright@gmail.com>
2022-02-06 09:33:14 -0800

    Fixed how we build dates in the test_utils

--- booking/tests/test_utils.py
+++ booking/tests/test_utils.py
@@ -5,16 +5,15 @@ from booking import utils
 
 @pytest.mark.django_db
 def test_check_bookings_with_date_range_with_some_results(booking_list):
-    start_date = datetime.datetime(2021, 8, 12).date()
-    end_date = datetime.datetime(2021, 8, 16).date()
+    start_date = datetime.date(2021, 8, 12)
+    end_date = datetime.date(2021, 8, 16)
     result = utils.check_bookings_with_date_range(start_date, end_date)
     assert len(result) == 2
 
 
 @pytest.mark.django_db
 def test_checking_bookings_with_date_range_no_overlap(booking_list):
-    start_date = datetime.datetime(2021, 10, 12).date()
-    end_date = datetime.datetime(2021, 10, 16).date()
+    start_date = datetime.date(2021, 10, 12)
+    end_date = datetime.date(2021, 10, 16)
     result = utils.check_bookings_with_date_range(start_date, end_date)
-    print(result[0])
     assert len(result) == 3
