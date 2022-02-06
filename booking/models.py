75a5706ced5d246104a0edfe29471d01e75ee7fa
Josh Bright <josh.bright@gmail.com>
2022-02-05 16:00:32 -0800

    Added a status field to the bookings model, removed the is_active flag

--- booking/models.py
+++ booking/models.py
@@ -5,10 +5,18 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Booking(models.Model):
+    booking_choices = (
+        ("new", "New"),
+        ("scheduled", "Scheduled"),
+        ("checked_in", "Checked-In"),
+        ("completed", "Completed"),
+    )
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
-    is_active = models.BooleanField()
+    status = models.CharField(
+        max_length=20, choices=booking_choices, default=booking_choices[0][0]
+    )
     check_in_time = models.DateTimeField(blank=True, null=True)
     room = models.OneToOneField(
         "rooms.Room", on_delete=models.CASCADE, blank=True, null=True
